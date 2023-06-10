aa9de8d8a9bd2bd082cc1e0cf622a86b35422b5e
RamsessVII <snacevedo2000@gmail.com>
2023-06-10 20:27:30 -0300

    Agrego el ejercicio 2 llamado miNombreObjeto.js

---
+++ practico1/miNombreObjeto.js
@@ -0,0 +1,9 @@
+//----------Ejercicio 2-------------------------
+//----------2.a-------------------------
+let persona = { 
+    nombre: "Santiago", 
+    apellido: "Acevedo", 
+    edad: 22
+};
+//----------2.b------------------------------
+console.log("Mi objeto persona es el siguiente: ", JSON.stringify(persona));

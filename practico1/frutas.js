21962db6bdb39e4f7506288acd49a1952d2f9138
RamsessVII <snacevedo2000@gmail.com>
2023-06-10 20:29:03 -0300

    agrego el ejercicio 5 llamado frutas.js

---
+++ practico1/frutas.js
@@ -0,0 +1,24 @@
+//------------ejercicio 5------------------------- 
+//-------5.a--------------
+let frutas = [] ;
+
+//--------5.b-----------------
+
+frutas.push("Manzana");
+frutas.push("Banana");
+frutas.push("KIwi");
+frutas.push("Durazno");
+frutas.push("Uva");
+
+//--------5.c---------------
+
+let ultima = frutas[frutas.length-1]; 
+
+console.log("La última fruta agregada fue:", ultima);
+
+//----------5.d------------------------
+
+let primera = frutas[0];
+
+console.log("La primera fruta agregada fue:", primera);
+
